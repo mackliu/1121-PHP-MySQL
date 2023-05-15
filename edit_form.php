866ca8425f4e64e77edaba5ba6072cd4e6e30039
kai <cgk0910@gmail.com>
2023-05-15 08:57:15 +0800

    建立編輯資料表單,並將id資料帶入表單的欄位中

--- edit_form.php
+++ edit_form.php
@@ -1,3 +1,69 @@
 <?php
 
-echo $_GET['id'];
+/* echo $_GET['id']; */
+$dsn="mysql:host=localhost;charset=utf8;dbname=student";
+$pdo=new PDO($dsn,'root','');
+$sql="select * from `students` where `id`='{$_GET['id']}'";
+$row=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編輯學生資料</title>
+</head>
+<body>
+<h1>編輯學生資料</h1>
+<form action="edit.php" method="post">
+
+<div>
+    <lable>學號</lable>
+    <input type="text" name="uni_id" value="<?=$row['uni_id'];?>">
+</div>
+<div>
+    <lable>班級座號</lable>
+    <input type="text" name="seat_num" value="<?=$row['seat_num'];?>">
+</div>
+<div>
+    <lable>姓名</lable>
+    <input type="text" name="name" value="<?=$row['name'];?>">
+</div>
+<div>
+    <lable>出生年月日</lable>
+    <input type="text" name="birthday" value="<?=$row['birthday'];?>">
+</div>
+<div>
+    <lable>身分證號碼</lable>
+    <input type="text" name="national_id" value="<?=$row['national_id'];?>">
+</div>
+<div>
+    <lable>住址</lable>
+    <input type="text" name="address" value="<?=$row['address'];?>">
+</div>
+<div>
+    <lable>家長</lable>
+    <input type="text" name="parent" value="<?=$row['parent'];?>">
+</div>
+<div>
+    <lable>電話</lable>
+    <input type="text" name="telphone" value="<?=$row['telphone'];?>">
+</div>
+<div>
+    <lable>科別</lable>
+    <input type="text" name="major" value="<?=$row['major'];?>">
+</div>
+<div>
+    <lable>畢業國中</lable>
+    <input type="text" name="secondary" value="<?=$row['secondary'];?>">
+</div>
+<div>
+    <input type="submit" value="編輯"><input type="reset" value="取消">
+</div>
+
+
+</form>
+</body>
+</html>
