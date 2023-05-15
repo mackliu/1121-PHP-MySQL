1bbd48fd06eb09d593b08e42229e7b5012bf4304
kai <cgk0910@gmail.com>
2023-05-15 10:38:51 +0800

    建立登入畫面

---
+++ login_form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員登入</title>
+</head>
+<body>
+<h1>會員登入</h1>    
+<form action="login.php" method="post">
+<p>
+    <label for="acc">帳號:</label>
+    <input type="text" name="acc" id="acc">
+</p>
+<p>
+    <label for="">密碼:</label>
+    <input type="password" name="pw" id="pw">
+</p>
+<p><input type="submit" value="登入"></p>
+</form>
+</body>
+</html>
